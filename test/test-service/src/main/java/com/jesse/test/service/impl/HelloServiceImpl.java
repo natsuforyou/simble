36f138472c834c987ce8f56a1032e9053f40701b
钱嘉鑫 <natsu_foryou@live.cn>
2016-05-15 22:23:30 +0800

    [delete] remove target files

--- test/test-service/src/main/java/com/jesse/test/service/impl/HelloServiceImpl.java
+++ test/test-service/src/main/java/com/jesse/test/service/impl/HelloServiceImpl.java
@@ -5,9 +5,11 @@
 package com.jesse.test.service.impl;
 
 import com.jesse.test.service.HelloService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Jesse Qian on 16-5-13.
  */
+@Service
 public class HelloServiceImpl implements HelloService {
 }
