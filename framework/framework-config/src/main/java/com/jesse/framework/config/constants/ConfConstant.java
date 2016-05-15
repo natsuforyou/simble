6ae742668abd1db555c027d7b9d93d75c3f9c035
钱嘉鑫 <natsu_foryou@live.cn>
2016-05-15 17:14:49 +0800

    [modify] rename resourse dir

--- framework/framework-config/src/main/java/com/jesse/framework/config/constants/ConfConstant.java
+++ framework/framework-config/src/main/java/com/jesse/framework/config/constants/ConfConstant.java
@@ -10,7 +10,7 @@ public class ConfConstant {
 
     public static final String APP_CONF_PATH = "META-INF/app.properties";
 
-    public static final String BASE_CONF_PATH = "properties/base.properties";
+    public static final String BASE_CONF_PATH = "META-INF/base.properties";
 
     public static final String ZK_CONF_PATH = "properties/zk.properties";
 }
