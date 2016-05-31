eddfd0c9a72d9520be94c706c290d025d5d3abe7
钱嘉鑫 <natsu_foryou@live.cn>
2016-05-31 12:17:24 +0800

    [add] spring boot start

--- front/rick-front/src/main/java/com/rick/qian/front/controller/test/TestController.java
+++ front/rick-front/src/main/java/com/rick/qian/front/controller/test/TestController.java
@@ -4,37 +4,45 @@
 
 package com.rick.qian.front.controller.test;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.rick.qian.front.common.response.wrappers.ObjectResult;
 import com.rick.qian.test.api.HelloApi;
-import jdk.nashorn.internal.ir.annotations.Reference;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 /**
  * Created by Rick Qian on 16-5-30.
  */
-
+@ImportResource("classpath:spring/rick-front-*.xml")
+@RestController
+@EnableAutoConfiguration
 @RequestMapping("/test")
-@Controller
-public class TestController {
+public class TestController implements EmbeddedServletContainerCustomizer {
 
-    @Reference
+//    @Reference
     @Resource
     private HelloApi helloApi;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    @ResponseBody
     public ObjectResult<String> hello() {
         String hello = helloApi.sayHello();
         return ObjectResult.success("操作成功", hello);
     }
 
     public static void main(String[] args) {
-
+        SpringApplication.run(TestController.class, args);
     }
 
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(8081);
+    }
 }
