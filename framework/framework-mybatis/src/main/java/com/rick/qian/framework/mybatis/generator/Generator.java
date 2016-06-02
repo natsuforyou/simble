8f0fcc8c721fe9be2e08754809af0d59ffc6abb7
钱嘉鑫 <natsu_foryou@live.cn>
2016-06-02 15:12:32 +0800

    [modify] mybatis generator

--- framework/framework-mybatis/src/main/java/com/rick/qian/framework/mybatis/generator/Generator.java
+++ framework/framework-mybatis/src/main/java/com/rick/qian/framework/mybatis/generator/Generator.java
@@ -6,35 +6,29 @@ package com.rick.qian.framework.mybatis.generator;
 
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
-import org.mybatis.generator.config.Context;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 public class Generator {
 
-    public static void main(String[] args) {
-        List<String> warnings = new ArrayList<>();
-        try {
-            boolean overwrite = true;
-            File configFile = new File("/home/jiaxin/Dev/simple-soa-website/framework/framework-mybatis/src/main/resources/mybatis/mapping/test_mapping.xml");
-            if (configFile.exists()) {
-                ConfigurationParser cp = new ConfigurationParser(warnings);
-                Configuration config = cp.parseConfiguration(configFile);
-                for (Context context : config.getContexts()) {
-                    context.addProperty("overwrite", String.valueOf(overwrite));
-                }
-                DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-                MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-                myBatisGenerator.generate(null);
-                System.out.println("Success");
-            } else
-                System.out.println("沒有找到配置文件");// 配置文件中的项目地址和包引用地址要用绝对地址，否则不能生成
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws Exception {
+
+        Resource resource = new PathMatchingResourcePatternResolver().getResource("mybatis/mapping/member_mapping.xml");
+
+        if (resource.exists()) {
+
+            Configuration config = new ConfigurationParser(null).parseConfiguration(resource.getInputStream());
+
+            config.getContexts().stream()
+                    .forEach(context -> context.addProperty("overwrite", Boolean.TRUE.toString()));
+
+            new MyBatisGenerator(config, new DefaultShellCallback(true), null).generate(null);
+
+            System.out.println("Success");
+        } else {
+            System.out.println("沒有找到配置文件");
         }
     }
 }
